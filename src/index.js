1a451958d58a4e65cfedf51a6b60bb14cd9badef
fifax23 <galaxymexx@gmail.com>
2021-08-10 02:50:48 +0600

    v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const bracketsStore = Object.fromEntries(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+    const topElement = stack[stack.length - 1];
+    if (bracketsStore[topElement] === str[i]) {
+      stack.pop();
+    } else if (bracketsStore[str[i]]) {
+      stack.push(str[i]);
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0 ? true : false;
+};
